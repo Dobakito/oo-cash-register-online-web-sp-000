cd6b0a9d977a04e13524786d433f4541c09eaf23
Michael Dobak <mdobak44@icloud.com>
2021-02-25 17:53:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -11,7 +11,7 @@ class CashRegister
   def add_item(item, price, quantity = 1)
     @price = price
     @total += price * quantity
-    @items << iteml
+    @items << item
   end
   
   def apply_discount
